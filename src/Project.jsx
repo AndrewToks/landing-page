30b7b3a9bb3d77581fe08342d07cdab1ab6243ed
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-01 21:51:11 +0100

    Projects and Service Section

--- src/Project.jsx
+++ src/Project.jsx
@@ -6,14 +6,14 @@ const Project = () => {
       <div>
         <h1 className='text-[24px] lg:text-[45px] text-center -mt-3'>Explore Some Of Our Projects</h1>
         <div className='lg:max-w-[900px] flex justify-end items-end flex-col ml-auto mr-auto mt-10 p-5'>
-        <small className='text-[#EDAB42] text-sm underline mb-5'>See all</small>
+        <small className='text-[#EDAB42]  text-sm underline mb-5'>See all</small>
         <div className='mr-auto ml-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px]'>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./p10.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
       </div>
       </div>
       </div>
---
+++ src/Testimony.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testimony = () => {
+  return (
+    <section>
+        <div>
+            <h1>Here's What Our Satisfied Clients Are Saying</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Testimony
