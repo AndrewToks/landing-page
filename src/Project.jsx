095f3f4b6ba814424f65543c32dfcc352cff3c3e
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-03 00:54:48 +0100

    footer Section

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PiArrowCircleUpLeftThin } from "react-icons/pi"
 
 const Project = () => {
   return (
@@ -8,12 +9,30 @@ const Project = () => {
         <div className='lg:max-w-[900px] flex justify-end items-end flex-col ml-auto mr-auto mt-10 p-5'>
         <small className='text-[#EDAB42]  text-sm underline mb-5'>See all</small>
         <div className='mr-auto ml-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px]'>
+          <div className='relative'>
           <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
+          <div className='relative'>
+          <img src="./room2.PNG" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
+          <div className='relative'>
           <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
+          <div className='relative'>
           <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
+          <div className='relative'>
           <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
+          <div className='relative'>
           <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
-          <img src="./room1.jpg" alt="" width={250} height={250} className='w-[300px] h-[250px]'/>
+          <PiArrowCircleUpLeftThin  className='absolute bottom-2 left-2' size={25}/>
+          </div>
       </div>
       </div>
       </div>
