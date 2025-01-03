fcad92ede54738a61ea57c3eaeaa2888af088ec0
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-03 01:20:05 +0100

    Projects and Service Section

--- src/ShowCase.jsx
+++ src/ShowCase.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { IoIosArrowRoundForward } from "react-icons/io"
 const ShowCase = () => {
   return (
     <section className='container flex flex-col gap-16 p-5 mt-24'>
@@ -11,7 +11,10 @@ const ShowCase = () => {
                 </h2>
                 <small className='text-[17px]'>Customized spaces that capture your personality,lifestyle and aesthetic
                 designed to inspire and enhance your daily life. </small>
-                <button className='bg-[#EDAB42] w-[250px] h-[35px] rounded-[7px]'>Learn More</button>
+                <div className='bg-[#EDAB42] w-[250px] h-[35px] rounded-[7px] flex justify-center items-center gap-10'>
+                <button>Learn More</button>
+                <IoIosArrowRoundForward size={20} />
+            </div>
             </div>
         </article>
         <article className='max-w-[900px] flex-col flex lg:flex-row justify-center items-center ml-auto mr-auto gap-2 lg:gap-[72px]'>
@@ -24,7 +27,10 @@ const ShowCase = () => {
                     beauty and wellness,empowering individuals to thrive in body, mind
                     and spirit
                 </small>
-                <button className='bg-[#EDAB42] w-[250px] h-[35px] rounded-[7px]'>Learn More</button>
+                <div className='bg-[#EDAB42] w-[250px] h-[35px] rounded-[7px] flex justify-center items-center gap-10'>
+                <button>Learn More</button>
+                <IoIosArrowRoundForward size={20} />
+            </div>
             </div>
             <img src="./image1.png" alt="" className='w-[511px] h-[400px]' width={500} height={500}/>
         </article>
